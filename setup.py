47a53f00cff93566734ec56b67586315cae634e6
Yuriy Sverchkov <yuriy@biostat.wisc.edu>
2022-12-05 16:31:21 -0600

    migrated readme to rst and added it to the package description

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 """Package setup"""
 from setuptools import setup
 
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='stringdb_alias',
     version='1.0',
@@ -8,6 +11,8 @@ setup(
     author_email='yuriy.sverchkov@wisc.edu',
     description=
         'A python package for working with string-db.org aliases (gene and protein ID mapping).',
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
     url='https://github.com/Craven-Biostat-Lab/stringdb_alias',
     license='MIT',
     packages=['stringdb_alias'],
